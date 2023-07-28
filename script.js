87b9067cedfb4232cb36de589d5fe8d6b2130b90
coffecoderboy <121314113+coffecoderboy@users.noreply.github.com>
2023-07-28 14:05:27 +0000

    finished the script

--- script.js
+++ script.js
@@ -28,5 +28,68 @@ function handleSymbol(symbol){
             previousOperator = null;
             buffer = runningTotal;
             runningTotal = 0;
+            break;
+        case '←':
+            if(buffer.length ===1){
+                buffer = '0';
+            }else{
+                buffer = buffer.toString(0, buffer.length -1);
+            }
+            break;
+        case '.':
+            buffer = parseFloat(buffer);
+
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+        }
+}
+
+function handleMath(symbol) {
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
     }
 }
+
+function handleNumber(numberString){
+    if(buffer === "0"){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-buttons').
+    addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+
+init();
